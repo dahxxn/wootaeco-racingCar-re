9a1d08db3ea33e9b741efb2dadf8fbca6d829311
daxxn <dahyeonhan4529@gmail.com>
2024-12-12 04:23:33 +0900

    feat(service): 실행결과 출력 코드 추가

--- src/main/java/com/example/racingcarre/service/Racing.java
+++ src/main/java/com/example/racingcarre/service/Racing.java
@@ -11,54 +11,56 @@ import java.util.List;
 public class Racing {
     TotalCar cars;
     int roundCounts;
+    OutputView outputView = new OutputView();
 
     public Racing(TotalCar cars, int roundCounts) {
         this.cars = cars;
         this.roundCounts = roundCounts;
     }
 
-    public void runRacing(){
+    public void runRacing() {
         OutputView outputView = new OutputView();
         outputView.printMessage("실행결과");
-        for(int i = 0; i < roundCounts; i++){
+        for (int i = 0; i < roundCounts; i++) {
             runRound();
         }
     }
 
-    public void runRound(){
+    public void runRound() {
         for (Iterator<Car> it = cars.getAllCar(); it.hasNext(); ) {
             Car car = it.next();
             carTurn(car);
         }
+        outputView.printCurrentRound(cars.getAllCar());
     }
 
-    public void carTurn(Car car){
-        if(isFastForward()){
+    public void carTurn(Car car) {
+        if (isFastForward()) {
             car.addDistance();
         }
     }
 
-    public boolean isFastForward(){
+    public boolean isFastForward() {
         return generateRandomState() >= 4;
     }
 
-    private int generateRandomState(){
-        return Randoms.pickNumberInRange(0,9);
+    private int generateRandomState() {
+        return Randoms.pickNumberInRange(0, 9);
     }
 
-    public List<String> getWinners(){
+    public List<String> getWinners() {
         List<String> winners = new ArrayList<>();
         Iterator<Car> totalCars = cars.getAllCar();
         long max = 0;
-        while(totalCars.hasNext()){
+        while (totalCars.hasNext()) {
             Car car = totalCars.next();
             long currentDistance = car.getDistance();
-            if(currentDistance > max){
+            if (currentDistance > max) {
                 max = currentDistance;
                 winners.add(car.getName());
                 continue;
             }
-            if(currentDistance == max){
+            if (currentDistance == max) {
                 winners.add(car.getName());
             }
         }
