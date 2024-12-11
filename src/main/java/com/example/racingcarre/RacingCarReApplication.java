de286ba18fe3e655b959a672f087eed56de983a5
daxxn <dahyeonhan4529@gmail.com>
2024-12-12 01:20:24 +0900

    feat: first commit

---
+++ src/main/java/com/example/racingcarre/RacingCarReApplication.java
@@ -0,0 +1,13 @@
+package com.example.racingcarre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacingCarReApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RacingCarReApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/racingcarre/RacingCarReApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.racingcarre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacingCarReApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
