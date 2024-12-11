5353fe7dbf07cb11be6ccc4fc7bc4126f17d5089
daxxn <dahyeonhan4529@gmail.com>
2024-12-12 02:56:40 +0900

    feat(view): 출력 담당 클래스 생성

---
+++ src/main/java/com/example/racingcarre/view/OutputView.java
@@ -0,0 +1,11 @@
+package com.example.racingcarre.view;
+
+public class OutputView {
+    public void printLineBreak() {
+        System.out.println();
+    }
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+}
