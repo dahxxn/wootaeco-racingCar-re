c3e1ac9465706d00d83d3cf600405aa3e7e6f5fd
daxxn <dahyeonhan4529@gmail.com>
2024-12-12 04:04:20 +0900

    feat(view): 실행 결과 관련 출력 메서드 생성

--- src/main/java/com/example/racingcarre/view/OutputView.java
+++ src/main/java/com/example/racingcarre/view/OutputView.java
@@ -1,5 +1,8 @@
 package com.example.racingcarre.view;
 
+import com.example.racingcarre.entity.Car;
+import java.util.Iterator;
+
 public class OutputView {
     public void printLineBreak() {
         System.out.println();
@@ -8,4 +11,28 @@ public class OutputView {
     public void printMessage(String message) {
         System.out.println(message);
     }
+
+    public String makeLineByDistance(long distance){
+        StringBuilder sb = new StringBuilder();
+        for(long i = 0 ; i<distance ; i++){
+            sb.append("-");
+        }
+        return sb.toString();
+    }
+
+    public void printCurrentRound(Iterator<Car> currentCars){
+        while (currentCars.hasNext()) {
+            Car car = currentCars.next();
+            car.printState();
+        }
+        printLineBreak();
+    }
+
+    public void printCarState(String name, long distance){
+        String CAR_STATE_FORMAT = "%s : %s";
+        String distanceVisual = makeLineByDistance(distance);
+        System.out.printf(CAR_STATE_FORMAT,name, distanceVisual);
+        printLineBreak();
+    }
+
 }
