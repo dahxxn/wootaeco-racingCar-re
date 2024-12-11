ad7e185374cd772eed8ce5fb49df00b59edfe057
daxxn <dahyeonhan4529@gmail.com>
2024-12-12 02:58:26 +0900

    feat(app): 메인 메서드 초기 선언

--- src/main/java/com/example/racingcarre/Application.java
+++ src/main/java/com/example/racingcarre/Application.java
@@ -1,8 +1,12 @@
 package com.example.racingcarre;
 
+import com.example.racingcarre.controller.Controller;
+
 public class Application {
 
     public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.run();
     }
 
 }
